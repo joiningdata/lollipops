27206d50df72bf4e08494e0b94a605f7172c050f
Jeremy Jay <jeremy@pbnjay.com>
2016-02-01 11:39:27 -0500

    adding GPL header to code files

--- main.go
+++ main.go
@@ -1,3 +1,23 @@
+//
+//    Lollipops command-line diagram generator for genetic variations.
+//    Copyright (C) 2015 Jeremy Jay <jeremy@pbnjay.com>
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+// Command lollipops provides a command-line interface for generating lollipop-style
+// genetic variation diagrams. It should be suitable for various scriptable pipelines
+// and other internal workflows.
 package main
 
 import (
